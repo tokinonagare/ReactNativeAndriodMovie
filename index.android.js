85a976d1dd6d1ab5221b733f4e6052d23af07073
tokinonagare <timemahou@gmail.com>
2015-09-22 18:04:59 +0800

    Fetching real data

---
+++ index.android.js
@@ -0,0 +1,104 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+/**
+* For quota reasons we replaced the Rotten Tomatoes' API with a sample data of
+* their very own API that lives in React Native's GitHub repo.
+*/
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+
+var React = require('react-native');
+var {
+  AppRegistry,
+  Image,
+  StyleSheet,
+  Text,
+  View,
+} = React;
+
+var MOCKED_MOVIES_DATA = [
+  {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
+];
+
+var AwesomeProject = React.createClass({
+  getInitialState: function() {
+      return{
+        movies: null,
+      };
+    },
+  componentDidMount: function() {
+    this.fetchData();
+  },
+  fetchData: function() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          movies: responseData.movies,
+        });
+      })
+      .done();
+  },
+  render: function() {
+    if (!this.state.movies) {
+      return this.renderLoadingView();
+    };
+
+    var movie = this.state.movies[0];
+
+    return this.renderMovie(movie);
+  },
+  renderLoadingView: function() {
+    return (
+      <View style={styles.container}>
+      <Text>
+        Loading movies...
+      </Text>
+      </View>
+    );
+  },
+  renderMovie: function(movie) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{uri: movie.posters.thumbnail}}
+          style ={styles.thumbnail}
+        />
+        <View Style={styles.rightContainer}>
+          <Text style={styles.title}>{movie.title}</Text>
+          <Text style={styles.year}> {movie.year}</Text>
+        </View>
+      </View>
+    );
+  },
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  rightContainer: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 8,
+    textAlign: 'center',
+  },
+  year: {
+    textAlign: 'center',
+  },
+  thumbnail: {
+    width: 53,
+    height: 81,
+  },
+});
+
+AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
