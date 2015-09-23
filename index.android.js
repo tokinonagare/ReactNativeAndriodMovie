c8ab1682f102fe792ecef82370a86d32ebce2810
tokinonagare <timemahou@gmail.com>
2015-09-23 15:28:11 +0800

    Add listView & adjust style

--- index.android.js
+++ index.android.js
@@ -8,58 +8,69 @@
 * For quota reasons we replaced the Rotten Tomatoes' API with a sample data of
 * their very own API that lives in React Native's GitHub repo.
 */
-var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
 
 var React = require('react-native');
 var {
   AppRegistry,
   Image,
+  ListView,
   StyleSheet,
   Text,
   View,
 } = React;
 
-var MOCKED_MOVIES_DATA = [
-  {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
-];
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
 
 var AwesomeProject = React.createClass({
   getInitialState: function() {
       return{
-        movies: null,
+        dataSource: new ListView.DataSource({
+          rowHasChanged: (row1, row2) => row1 !== row2,
+        }),
+        loaded: false,
       };
     },
+
   componentDidMount: function() {
     this.fetchData();
   },
+
   fetchData: function() {
     fetch(REQUEST_URL)
       .then((response) => response.json())
       .then((responseData) => {
         this.setState({
-          movies: responseData.movies,
+          dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
+          loaded: true,
         });
       })
       .done();
   },
+
   render: function() {
-    if (!this.state.movies) {
+    if (!this.state.loaded) {
       return this.renderLoadingView();
     };
 
-    var movie = this.state.movies[0];
-
-    return this.renderMovie(movie);
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow ={this.renderMovie}
+        style     ={styles.listView}
+      />
+    );
   },
+
   renderLoadingView: function() {
     return (
-      <View style={styles.container}>
+      <View style={styles.loadingContainer}>
       <Text>
         Loading movies...
       </Text>
       </View>
     );
   },
+
   renderMovie: function(movie) {
     return (
       <View style={styles.container}>
@@ -80,24 +91,38 @@ var styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: 'flex-start',
     backgroundColor: '#F5FCFF',
   },
+
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
   rightContainer: {
     flex: 1,
   },
+
   title: {
     fontSize: 20,
     marginBottom: 8,
-    textAlign: 'center',
+    textAlign: 'left',
   },
+
   year: {
-    textAlign: 'center',
+    textAlign: 'left',
   },
+
   thumbnail: {
     width: 53,
     height: 81,
+  },
+
+  listView: {
+    paddingTop: 20,
+    backgroundColor: '#F5FCFF',
   },
 });
 
